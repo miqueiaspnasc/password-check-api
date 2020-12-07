9ed3b91ae1b71e58416050caffbc03265f401d52
Miqueias Nascimento <miqueias.p.nasc@gmail.com>
2020-12-06 21:16:51 -0300

    Password validation service implementation

--- src/main/kotlin/br/com/credentials/service/PasswordValidatorServiceImpl.kt
+++ src/main/kotlin/br/com/credentials/service/PasswordValidatorServiceImpl.kt
@@ -2,6 +2,12 @@ package br.com.credentials.service
 
 class PasswordValidatorServiceImpl: PasswordValidatorService {
     override fun isValid(password: String): Boolean {
-        TODO("Not yet implemented")
+        return !password.contains("\\s".toRegex())
+                && password.length >= 9
+                && password.toSet().distinct().size == password.length
+                && password.contains("\\d".toRegex())
+                && password.contains("[A-Z]".toRegex())
+                && password.contains("[a-z]".toRegex())
+                && password.contains("[!@#$%^&*()\\-+]".toRegex())
     }
 }
